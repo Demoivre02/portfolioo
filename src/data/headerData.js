d856c344ea5c15e5c0ea2944bb4f962301f33065
David <davidtimilehin739@gmail.com>
2023-09-06 11:06:14 +0100

    fix image issue

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/Animasaun David 001.pdf'
+import myimage from '../assets/png/me.jpg'
 
 export const headerData = {
     name: 'Animasaun David',
     title: "Frontend Engineer",
     desciption:"An intelligent, contemplative individual who excels in crafting exceptional websites that prioritize responsiveness and deliver a seamless user experience. I consistently prioritize user needs and are deeply committed to optimizing application performance and enhancing security.",
-    image: 'src/assets/png/me.jpg',
+    image: myimage,
     resumePdf: resume
 }
